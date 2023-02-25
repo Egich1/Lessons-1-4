a23166257bbba689589301511ce2975fec607ded
Egich1 <95471267+Egich1@users.noreply.github.com>
2023-02-25 10:43:06 +0300

    Add files via upload

---
+++ nod/mocha/operations.js
@@ -0,0 +1,13 @@
+//MOCHA
+
+// module.exports.multiply = function(x,y){return x * y;}
+// module.exports.add = function(x, y){ return x + y;}
+
+
+//ASSERT
+// module.exports.multiply = function(x,y){return x * y;}
+
+
+//Describe
+module.exports.multiply = function(x,y){return x * y;}
+module.exports.add = function(x, y){ return x + y;}
